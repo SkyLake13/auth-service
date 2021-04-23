edadc538e422a95f83497f058a41354414a0e02e
kumawabh <kumawabh@WL350183>
2021-04-23 18:34:44 +0530

    removed mongoose types

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { connect, connection } from 'mongoose';
 import { login } from './login/login';
 import { signup } from './signup/signup';
 
+const PORT = process.env.PORT || 8090;
+
 connect(`mongodb://az-900:1XvtzhgnTnqTk6QBk2m8kAvYC95TaNgQzEM7g6yEblgDLMy591OGLleKvDT3STp5eJ6o1oa0FQE4YtRsZxiQ0g==@az-900.mongo.cosmos.azure.com:10255/userdb?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@az-900@`,
          { useNewUrlParser: true });
 connection.on('error', (error) => console.error(error));
@@ -17,4 +19,4 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/login', login);
 app.use('/signup', signup);
 
-app.listen(8090, () => console.log('app started.'));
+app.listen(PORT, () => console.log(`app started at port ${PORT}`));
